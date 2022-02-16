c356cd56821680098befaa3abbd774e74a846050
proinfo09 <63229875+proinfo09@users.noreply.github.com>
2022-02-16 15:44:31 +0700

    Add asset

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+    public WheelCollider[] wheels = new WheelCollider[4];
+    public float torque = 200;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            for(int i = 0; i < wheels.Length; i++)
+            {
+                wheels[i].motorTorque = torque;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < wheels.Length; i++)
+            {
+                wheels[i].motorTorque = 0;
+            }
+        }
     }
 }
